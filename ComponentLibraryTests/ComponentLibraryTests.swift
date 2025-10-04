244c684d1e784cfc8deea0dac95913b2c277e07f
cliftonia <clifton.baggerman@me.com>
2025-10-04 23:08:51 +1000

    Reorganize ComponentLibrary files for Xcode target
    - Moved showcase files to root
    - Moved Components folder to root
    - Removed default Xcode template files
    - Ready to build and run

---
+++ ComponentLibraryTests/ComponentLibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentLibraryTests.swift
+//  ComponentLibraryTests
+//
+//  Created by Clifton Baggerman on 04/10/2025.
+//
+
+import Testing
+@testable import ComponentLibrary
+
+struct ComponentLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
