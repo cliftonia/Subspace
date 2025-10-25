fb346a1d8827837810db07ad6750e25cea7c4c86
cliftonia <clifton.baggerman@me.com>
2025-10-25 23:20:42 +1000

    Fix Package.swift trailing comma syntax

--- LCARSComponents/Package.swift
+++ LCARSComponents/Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "LCARSComponents",
     platforms: [
         .iOS(.v17)
-    ]
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LCARSComponents",
             targets: ["LCARSComponents"]
-        ),
-    ]
+        )
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -24,6 +24,6 @@ let package = Package(
         .testTarget(
             name: "LCARSComponentsTests",
             dependencies: ["LCARSComponents"]
-        ),
+        )
     ]
 )
